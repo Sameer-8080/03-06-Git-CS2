d82cccb79d6d498e6da801c63f3f171434b3ab7d
Sameer <109897419+Sameer-8080@users.noreply.github.com>
2025-01-30 20:43:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,19 +4,13 @@ Initial Function()
 {
   //Sample Code
 }
-
-
 Initial Security()
 {
  //Sample Code
 }
-
-
 feaature2()
 {
   //Sample Code for Feature 2
 }
-
-
-
 }
+//Changes made to show difference between fetch and pull
